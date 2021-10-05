dbc2abf9c5945247774c37ab007edc663eca4f2d
PatoBaigorria <87587522+PatoBaigorria@users.noreply.github.com>
2021-10-05 00:04:21 -0300

    Subi los archivos

---
+++ src/tp6/DirectorioTelefonico.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Patricia
+ */
+public class DirectorioTelefonico {
+    private TreeMap<String, Cliente> lista=new TreeMap<>();
+
+    public boolean agregarCliente(String tel, Cliente c){
+        if(lista.containsKey(tel)){
+            return false;
+        }else{
+            lista.put(tel, c);
+            return true;
+        }        
+    }
+    
+    public Cliente buscarCliente(String tel){
+        return lista.get(tel);
+    }
+    
+    public ArrayList<String> buscarTelefono(String apellido){
+        ArrayList<String> devlista = new ArrayList<>();
+        
+        Set<String>telclave=lista.keySet();
+        
+        for(String it : telclave){
+            Cliente c = lista.get(it);
+            if (c.getApellido().equals(apellido)){
+                devlista.add(it);
+            }
+        }
+        return devlista;
+    }
+    
+    public ArrayList<Cliente> buscarClientes(String ciudad){
+        ArrayList<Cliente>devclientes = new ArrayList<>();
+        
+        Set<String>ciudclave=lista.keySet();
+        
+        for(String it : ciudclave){
+            Cliente c = lista.get(it);
+            if (c.getCiudad().equals(ciudad)){
+                devclientes.add(c);
+            }
+        }
+        return devclientes;
+       
+    }
+    
+    
+    public Cliente borrarCliente (long dni){
+    
+        Set<String> dniclaves=lista.keySet();
+        
+        for (String it:dniclaves){
+            Cliente c=lista.get(it);
+            
+            if (c.getDni() == dni){
+                lista.remove(it);
+                return c;
+            }
+            
+        }
+        return null;
+    }
+          
+}
--- src/tp6/Tp6.java
+++ src/tp6/Tp6.java
@@ -16,6 +16,30 @@ public class Tp6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        DirectorioTelefonico dir = new DirectorioTelefonico();
+        
+        Cliente c1 = new Cliente (27013989, "Juan", "López", "Córdoba", "Granada 2761");
+        Cliente c2 = new Cliente (24120983, "Soledad", "Suárez", "Mendoza", "Vascongadas 5726");
+        Cliente c3 = new Cliente (11249586, "Pablo", "Romero", "San Juan", "Galicia 1298");
+        Cliente c4 = new Cliente (20749823, "Carla", "Toledo", "Jujuy", "Moreno 4417");
+        
+        dir.agregarCliente("0351-482813", c1);
+        dir.agregarCliente("2664-258159", c2);
+        dir.agregarCliente("0261-357486", c3);
+        dir.agregarCliente("0388-173749", c4);
+        
+        
+        
+        System.out.println(dir.buscarCliente("2664-258159"));
+        
+        System.out.println(dir.buscarClientes("Córdoba"));
+        
+        System.out.println("El número de Teléfono de " + c3.getApellido()+" es: "+ dir.buscarTelefono("Romero"));
+        
+        System.out.println("El Cliente "+dir.borrarCliente(20749823)+" fue ELIMINAD@ CON ÉXITO");
+        
+        System.out.println("El Cliente no Existe"+dir.buscarTelefono("Toledo"));
+        
     }
     
 }
